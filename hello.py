344fb04f85fcbd01d0af99ee8f080f598cf5b59c
Lukas <l.noorlaid@voco.ee>
2022-10-21 09:50:25 +0300

    Ülesanne: Hello world! - docstring ja PEP

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
-print("Hello world!") #siin me prindime Hello world!
+""" At, first we wrote print ("Hello world!") and after that we added a comment # Here we print Hello world! """
+print("Hello world!")  # Here we print Hello world!

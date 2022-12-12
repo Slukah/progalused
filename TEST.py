92bf0c4df1443fa07f09ee1ba2e3421e0fd35e24
Lukas <l.noorlaid@voco.ee>
2022-12-12 13:47:10 +0200

    TEST: VARIANT 1 valmis ning töötab

--- TEST.py
+++ TEST.py
@@ -21,21 +21,21 @@ if len(ID) == 11:
             Gender = Genders[sex - 1]
         if 8 >= int(ID[0]) >= 1:
             if int(ID[0]) == 1 or int(ID[0]) == 2:
-                aasta = "18" + str(ID[1] + ID[2])
+                year = "18" + str(ID[1] + ID[2])
             if int(ID[0]) == 3 or int(ID[0]) == 4:
-                aasta = "19" + str(ID[1] + ID[2])
+                year = "19" + str(ID[1] + ID[2])
             if int(ID[0]) == 5 or int(ID[0]) == 6:
-                aasta = "20" + str(ID[1] + ID[2])
+                year = "20" + str(ID[1] + ID[2])
             if int(ID[0]) == 7 or int(ID[0]) == 8:
-                aasta = "21" + str(ID[1] + ID[2])
+                year = "21" + str(ID[1] + ID[2])
             for MonthName in months:
                 month = int(ID[3] + ID[4])
                 MonthName = months[month - 1]
             if 1 <= month <= 12:
                 day = int(ID[5] + ID[6])
                 if 1 <= day <= 31:
-                    print("Tervist, oled " + Gender + ". Sündisid aastal " + aasta + ", sünnipäev on " + str(
-                        day) + ". " + MonthName)
+                    print("Tervist, oled " + Gender + ". Sündisid aastal " + year + ", sünnipäev on " + str(
+                        day) + ". " + MonthName + ". ")
                 else:
                     print("Viga! Proovi uuesti!")
             else:
